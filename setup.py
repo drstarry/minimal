cae94a72da823504be2f632bc338e7f14d2e2947
Starry <drstarry@gmail.com>
2015-09-11 17:47:45 -0600

    add buildout staff

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'click',
+    'numpy',
+    'scipy',
+    'pandas',
+]
+
+entry_points = '''
+[console_scripts]
+minimal = minimal.cli.cli:cli
+'''
+
+setup(name='minimal',
+      version='0.0.1',
+      author='Rui Dai | Starry',
+      author_email='drstarry@gmail.com',
+      description='minimal',
+      license='PRIVATE',
+      packages=find_packages(),
+      install_requires=install_requires,
+      entry_points=entry_points)
