f190e6b17d1473ac98bdabb97f519de033bdb269
Starry <drstarry@gmail.com>
2015-09-29 14:34:04 -0600

    add batch perceptron

--- minimal/perceptron/const.py
+++ minimal/perceptron/const.py
@@ -8,6 +8,8 @@ FILE_TRAIN = 'train%i.%i'
 FILE_TEST = 'test%i.%i'
 FILE_NUM = range(2)
 FEATURE_NUM = range(10, 110, 10)
+
 # hyper parameters list
-MU = [0.01, 0.02, 0.05, 0.1, 0.3, 0.4]
-R = [0.01, 0.02, 0.05, 0.1, 0.15, 0.5]
+MU = [0.01, 0.02, 0.05, 0.1, 0.2, 0.3, 0.4, 0.8]
+R = [0.01, 0.02, 0.05, 0.1, 0.15, 0.5, 0.8, 1]
+EPOCH = 10
--- minimal/perceptron/perceptron.py
+++ minimal/perceptron/perceptron.py
@@ -1,10 +1,11 @@
 # coding: utf-8
 
 from random import shuffle, uniform
-import IPython
+# import IPython
 
 from const import CUR_FOLDER, FILE_FOLDER, FILE_TRAIN, FILE_TEST, FILE_NUM, \
-                FEATURE_NUM, MU, R
+    FEATURE_NUM, MU, R, EPOCH
+
 
 def prepare_data():
     data = dict()
@@ -12,6 +13,7 @@ def prepare_data():
         for j in FEATURE_NUM:
             data[(i, j)] = prepare_pair(i, j)
     return data
+
 
 def prepare_pair(i, j):
     """i -> data NO. (data0, data1)
@@ -32,20 +34,24 @@ def prepare_pair(i, j):
                     data[file_type].append((label, feature))
     return data
 
+
 def prepare_weight(j):
     """j -> feature number
     randomly generate weights for initial state
     return a j+1 vector"""
     return [uniform(0, 1) for _ in range(j+2)]
 
+
 def dot_prod(a, b):
     """a -> row vector
        b -> column vector"""
     return sum([a[i]*b[i] for i in range(len(a))])
 
+
 def update_weight(weight, feature, r, label):
     for idx, w in enumerate(weight[:]):
         weight[idx] += r*label*feature[idx]
+
 
 def train_pair(r, j, data, mu=0):
     """mu -> margin
@@ -61,6 +67,7 @@ def train_pair(r, j, data, mu=0):
             update_weight(weight=w, r=r, feature=feature, label=label)
     return w
 
+
 def test_pair(w, r, j, data, mu=0):
     """mu -> margin
        w -> weight
@@ -73,15 +80,38 @@ def test_pair(w, r, j, data, mu=0):
             mistake += 1
     return 1 - float(mistake)/len(data)
 
-def train_batch(data, j, mu=0, batch=False, cv=False):
-    """mu -> margin
-       batch -> whether to shuffle data
-       cv -> whether to do cross validation
-       j -> feature number
-    """
-    r = cross_validation()
-    w = prepare_weight(j)
-    pass
+
+def train_batch(data_all, epoch=EPOCH, mu_list=MU, r_list=R):
+    for i in FILE_NUM:
+        for j in FEATURE_NUM:
+            data = data_all[(i, j)]
+            train_data = data['train']
+            for r in r_list:
+                w = train_pair(data=data['train'], j=j, r=r)
+                for _ in range(epoch):
+                    shuffle(train_data)
+                    w = verctor_add(w, train_pair(data=train_data, j=j, r=r))
+                w = [_w/epoch for _w in w]
+                precision = test_pair(data=data['test'], j=j, r=r, w=w)
+                print 'data %i, feature number %i, learning rate %f, precision %f' \
+                    % (i, j, r, precision)
+
+            # try different mu
+            for mu in mu_list:
+                w = train_pair(data=data['train'], j=j, r=1)
+                for _ in range(epoch):
+                    shuffle(train_data)
+                    w = verctor_add(w, train_pair(mu=mu, data=train_data, j=j,
+                                                  r=1))
+                w = [_w/epoch for _w in w]
+                precision = test_pair(mu=mu, data=data['test'], j=j, r=1, w=w)
+                print 'data %i, feature number %i, mu %f, precision %f' \
+                    % (i, j, mu, precision)
+
+
+def verctor_add(w, w_):
+    return [w[i]+w_[i] for i in range(len(w))]
+
 
 def train_online(data_all):
     for i in FILE_NUM:
@@ -90,14 +120,16 @@ def train_online(data_all):
             w = train_pair(data=data['train'], j=j, r=1)
             precision = test_pair(data=data['test'], j=j, r=1, w=w)
             print 'data %i, feature number %i, precision %f' \
-                    % (i, j, precision)
+                % (i, j, precision)
             w = train_pair(mu=0.3, data=data['train'], j=j, r=1)
             precision = test_pair(data=data['test'], j=j, r=1, w=w)
             print 'data %i, feature number %i, mu %f, precision %f' \
-                    % (i, j, 0.3, precision)
+                % (i, j, 0.3, precision)
 
+
+def train():
+    data = prepare_data()
+    train_batch(data)
 
 if __name__ == "__main__":
-    data = prepare_data()
-    train_online(data)
-    # IPython.embed()
+    train()
